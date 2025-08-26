e0fa9eaf3bacb58861f5c54a047d59c6bc50c1a8
Ashwanth Saran JC <ashwanthsaranjc@gmail.com>
2025-08-27 00:06:06 +0530

    Add files via upload

---
+++ Deploy/user_management/urls.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+from django.urls import path, include, re_path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth import views as auth_views
+from users.views import CustomLoginView, ChangePasswordView
+
+from users.forms import LoginForm
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', include('users.urls')),
+
+    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
+                                           authentication_form=LoginForm), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+    path("home/",include("Chatbot.urls")),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
